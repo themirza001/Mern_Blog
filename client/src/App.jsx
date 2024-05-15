f8edbae858f9a8f482b47686cbbb90f22a12803d
themirza001 <themirza001@gmail.com>
2024-05-15 14:47:50 +0530

    Created Browser Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-800">Bismillah</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
