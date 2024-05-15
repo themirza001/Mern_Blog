ef22b0cadd87919dd363d2019cf8c9eedc2d0845
themirza001 <themirza001@gmail.com>
2024-05-15 16:32:13 +0530

    connecting to the database

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const port = 3000;
+
+mongoose
+  .connect('mongodb://localhost:27017/blogDataBase')
+  .then(() => {
+    console.log('DataBase connection is SuccessFull');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`server is Listening at port ${port}`);
