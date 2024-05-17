1f316e40182d4517b8136d2694c664d5c76fc79d
themirza001 <themirza001@gmail.com>
2024-05-17 16:28:16 +0530

    Adding the DashSide Bar UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,27 @@
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import DashSideBar from '../Components/DashSideBar';
+import DashProfile from '../Components/DashProfile';
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    // console.log(tabFromUrl);
+    if (tabFromUrl) setTab(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div>
+        {/* SideBar */}
+        <DashSideBar />
+      </div>
+      {/* profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 }
 
 export default Dashboard;
