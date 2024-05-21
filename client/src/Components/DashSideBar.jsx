de941eeaa673f6baf0b893ad5d918abc0d740293
themirza001 <themirza001@gmail.com>
2024-05-22 00:32:31 +0530

    Add Dash Board functionality

--- client/src/Components/DashSideBar.jsx
+++ client/src/Components/DashSideBar.jsx
@@ -1,17 +1,21 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useSelector } from 'react-redux';
 function DashSideBar() {
+  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
   const [tab, setTab] = useState('');
   const dispatch = useDispatch();
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
+
     const tabFromUrl = urlParams.get('tab');
+
     // console.log(tabFromUrl);
     if (tabFromUrl) setTab(tabFromUrl);
   }, [location.search]);
@@ -34,18 +38,29 @@ function DashSideBar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmRight}
             className="cursor-pointer"
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,16 +2,19 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import DashSideBar from '../Components/DashSideBar';
 import DashProfile from '../Components/DashProfile';
+import DashPosts from '../Components/DashPosts';
 
 function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
     // console.log(tabFromUrl);
     if (tabFromUrl) setTab(tabFromUrl);
   }, [location.search]);
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div>
@@ -20,6 +23,8 @@ function Dashboard() {
       </div>
       {/* profile */}
       {tab === 'profile' && <DashProfile />}
+      {/* posts */}
+      {tab === 'posts' && <DashPosts />}
     </div>
   );
 }
