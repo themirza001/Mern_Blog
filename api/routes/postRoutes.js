3390dcd788ab1fc8e9da64e013a0c9bb9f030a23
themirza001 <themirza001@gmail.com>
2024-05-22 01:03:14 +0530

    Adding the get Post API Route

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { verifyToken } = require('../utils/verifyUser');
-const { createPost } = require('./../controller/postController');
+const { createPost, getPosts } = require('./../controller/postController');
 const router = express.Router();
 
 router.route('/create').post(verifyToken, createPost);
+router.route('/getposts').get(getPosts);
 
 module.exports = router;
