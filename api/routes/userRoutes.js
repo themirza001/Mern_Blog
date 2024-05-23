f6ac4dde88e118cdde8b9e66e9326061a98f8fab
themirza001 <themirza001@gmail.com>
2024-05-23 14:11:18 +0530

    creating Get All users API Route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,4 +6,5 @@ const router = express.Router();
 router.route('/update/:userId').put(verifyToken, userController.updateUser);
 router.route('/delete/:userId').delete(verifyToken, userController.deleteUser);
 router.route('/signout').post(userController.signout);
+router.route('/getUsers').get(verifyToken, userController.getUsers);
 module.exports = router;
