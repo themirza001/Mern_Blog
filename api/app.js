0928a76fd4e3d7f15e53edf9926cd8e89dcd2ac9
themirza001 <themirza001@gmail.com>
2024-06-01 01:31:25 +0530

    Final Submiission

--- api/app.js
+++ api/app.js
@@ -6,11 +6,13 @@ const commentRouter = require('./routes/commentRoute');
 const AppError = require('./utils/AppError');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+// const __dirname = path.resolve();
 // const check = (req, res, next) => {
 //   console.log('Hello from the middlware');
 //   next();
@@ -20,6 +22,7 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/post', postRouter);
 app.use('/api/v1/comment', commentRouter);
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.all('*', (req, res, next) => {
